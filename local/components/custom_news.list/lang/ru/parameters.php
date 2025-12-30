e536568356b8726e1642aa83f6b89deb2982046e
dasha923 <dashayuditskaya@yandex.ru>
2025-12-31 00:30:19 +0700

    Delete local/components/custom_news.list/lang/ru/parameters.php

--- local/components/custom_news.list/lang/ru/parameters.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$MESS["T_IBLOCK_DESC_LIST_ID"] = "Код информационного блока";
-$MESS["T_IBLOCK_DESC_LIST_TYPE"] = "Тип информационного блока";
-$MESS["T_IBLOCK_PROPERTY"] = "Свойства";
-$MESS["T_IBLOCK_FILTER"] = "Фильтр";
-$MESS["IBLOCK_FIELD"] = "Поля";
-?>
